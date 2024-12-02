0b261e7030f2a20516b8fb9c8263255d99279ecd
Yutong Wang <yutong_wang5@brown.edu>
2024-12-02 16:11:14 -0500

    Update some files

---
+++ ccv-scripts/finetune.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH -p gpu --gres=gpu:2
+#SBATCH -n 2
+#SBATCH --time=24:00:00
+#SBATCH --mem=48G
+#SBATCH -J finetune-openvla
+
+#SBATCH -o finetune-openvla-%j.out
+#SBATCH -e finetune-openvla-%j.out
+
+# Run a command
+cd ..
+torchrun --standalone --nnodes 1 --nproc-per-node 1 vla-scripts/finetune.py --config_path=vla-scripts/config/finetune_robosuite.yaml
---
+++ eval_libero.sh
@@ -0,0 +1,7 @@
+python experiments/robot/libero/run_libero_eval.py \
+  --model_family openvla \
+  --pretrained_checkpoint openvla/openvla-7b-finetuned-libero-spatial \
+  --task_suite_name libero_spatial \
+  --center_crop True \
+  --num_trials_per_task 2 \
+  --load_in_4bit True
