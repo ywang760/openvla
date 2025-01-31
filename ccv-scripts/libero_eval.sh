2ae8da2c2175ad59dc5ee7e1d17747ffa88cb4b0
Yutong Wang <yutong_wang5@brown.edu>
2025-01-30 22:54:09 -0500

    Update ccv-scripts

--- ccv-scripts/libero_eval.sh
+++ ccv-scripts/libero_eval.sh
@@ -4,30 +4,26 @@
 #SBATCH -p gpu --gres=gpu:1
 #SBATCH -n 2
 #SBATCH --time=2:00:00
-#SBATCH --mem=24G
+#SBATCH --mem=96G
 #SBATCH -J libero-eval
 
 #SBATCH -o log/libero-eval-%j.out
 #SBATCH -e log/libero-eval-%j.err
 
 # Run a command
+bash prelaunch.sh
 cd ..
 
-# print the hostname and the current conda environment
-echo "Running on $(hostname)"
-nvidia-smi
-echo "Current conda environment: $CONDA_DEFAULT_ENV"
-
 # LoRA finetuned myself
-python experiments/robot/libero/run_libero_eval.py \
-  --model_family openvla \
-  --pretrained_checkpoint openvla/openvla-7b \
-  --lora_adapter True \
-  --lora_exp_id openvla-7b+libero_spatial+b8+lr-0.0005+lora-r32+dropout-0.0+q-4bit--None--image_aug \
-  --load_in_4bit True \
-  --center_crop True \
-  --task_suite_name libero_spatial \
-  --num_trials_per_task 20
+# python experiments/robot/libero/run_libero_eval.py \
+#   --model_family openvla \
+#   --pretrained_checkpoint openvla/openvla-7b \
+#   --lora_adapter True \
+#   --lora_exp_id openvla-7b+libero_spatial+b8+lr-0.0005+lora-r32+dropout-0.0+q-4bit--None--image_aug \
+#   --load_in_4bit True \
+#   --center_crop True \
+#   --task_suite_name libero_spatial \
+#   --num_trials_per_task 20
 
 # Launch LIBERO-Spatial evals
 python experiments/robot/libero/run_libero_eval.py \
@@ -36,7 +32,7 @@ python experiments/robot/libero/run_libero_eval.py \
   --task_suite_name libero_spatial \
   --center_crop True \
   --num_trials_per_task 3 \
-  --load_in_4bit True
+  --load_in_4bit False
 
 # # Launch LIBERO-Object evals
 # python experiments/robot/libero/run_libero_eval.py \
