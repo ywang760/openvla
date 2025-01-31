eaa6532541dd92a30b0e3391664404664611152a
Yutong Wang <yutong_wang5@brown.edu>
2025-01-30 22:59:58 -0500

    Update train pipeline

--- prismatic/vla/datasets/rlds/oxe/mixtures.py
+++ prismatic/vla/datasets/rlds/oxe/mixtures.py
@@ -206,5 +206,10 @@ OXE_NAMED_MIXTURES: Dict[str, List[Tuple[str, float]]] = {
     "libero_10_no_noops": [
         ("libero_10_no_noops", 1.0),
     ],
+
+    # === Robosuite Datasets ===
+    "robosuite": [
+        ("robosuite_dataset", 1.0),
+    ]
 }
 # fmt: on
